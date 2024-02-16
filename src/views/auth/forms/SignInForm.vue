f3ececeb3e18a8d50f8a3bb38712b265a180d0f5
Joel Filho <joelfilho02812@gmail.com>
2024-02-16 12:32:44 -0300

    feat: add sign-in form

---
+++ src/views/auth/forms/SignInForm.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import { ref, watch } from 'vue'
+import { Label } from '@/components/ui/label'
+import { Input } from '@/components/ui/input'
+import { Button } from '@/components/ui/button'
+
+const $emits = defineEmits<{
+  (e: 'sign-in'): void
+  (e: 'update:modelValue', value: Object): void
+}>()
+
+const form = ref({
+  email: ''
+})
+
+watch(
+  () => form.value,
+  (newValue) => {
+    if (newValue) {
+      $emits('update:modelValue', form.value)
+    }
+  },
+  { deep: true }
+)
+</script>
+
+<template>
+  <form @submit.prevent.stop="$emit('sign-in')">
+    <Label for="email">E-mail</Label>
+    <Input id="email" v-model:model-value="form.email" placeholder="E-mail" />
+
+    <div class="flex items-center justify-center mt-5">
+      <Button type="submit" class="w-full">Acessar Painel</Button>
+    </div>
+  </form>
+</template>
