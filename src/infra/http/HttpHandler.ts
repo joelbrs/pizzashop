6e48ffb2737254b92d4eab7bca55debde483eed9
Joel Filho <joelfilho02812@gmail.com>
2024-02-16 11:21:56 -0300

    refactor: add constructUrl in HttpHandler

--- src/infra/http/HttpHandler.ts
+++ src/infra/http/HttpHandler.ts
@@ -2,11 +2,21 @@ import { setDataResponse, setErrorResponse } from '../utils/ResponseHandler'
 import type HttpClient from './HttpClient'
 
 export default class HttpHandler {
-  constructor(private readonly _client: HttpClient) {}
+  constructor(
+    private readonly _client: HttpClient,
+    private readonly _baseUrl?: string
+  ) {}
+
+  private constructUrl(url: string) {
+    if (this._baseUrl) {
+      return `${this._baseUrl}/${url}`
+    }
+    return `${url}`
+  }
 
   async post<T>(url: string, body: Object) {
     try {
-      const data = await this._client.post<T>(url, body)
+      const data = await this._client.post<T>(this.constructUrl(url), body)
       return setDataResponse<T>(data)
     } catch (err) {
       return setErrorResponse(err as Error)
