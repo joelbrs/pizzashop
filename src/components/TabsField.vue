2eb2bafca824e05fb4e5faccebb3b8c749734346
Joel Filho <joelfilho02812@gmail.com>
2024-02-16 12:32:31 -0300

    feat: add tabs-field component

---
+++ src/components/TabsField.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
+
+export interface Tab {
+  id: string
+  label: string
+  title: string
+  description?: string
+}
+
+defineProps<{
+  tabs: Tab[]
+}>()
+</script>
+
+<template>
+  <Tabs :default-value="tabs[0].id" class="w-[25vw]">
+    <TabsList class="grid w-full grid-cols-2">
+      <TabsTrigger v-for="tab in tabs" :key="tab.id" :value="tab.id"> {{ tab.label }} </TabsTrigger>
+    </TabsList>
+    <TabsContent v-for="tab in tabs" :key="tab.id" :value="tab.id">
+      <Card>
+        <CardHeader>
+          <CardTitle class="text-xl font-semibold">{{ tab.title }}</CardTitle>
+          <CardDescription class="text-muted-foreground text-sm">
+            {{ tab.description }}
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <slot :name="`content-${tab.id}`" />
+        </CardContent>
+
+        <div class="flex items-center justify-center w-full">
+          <slot :name="`footer-${tab.id}`" />
+        </div>
+      </Card>
+    </TabsContent>
+  </Tabs>
+</template>
