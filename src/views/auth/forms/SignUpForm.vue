cbd263937fe88b474a97dfc85e86a2d6e9ce07a0
Joel Filho <joelfilho02812@gmail.com>
2024-02-16 12:33:00 -0300

    feat: add sign-up form

---
+++ src/views/auth/forms/SignUpForm.vue
@@ -0,0 +1,68 @@
+<script setup lang="ts">
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+import { Button } from '@/components/ui/button'
+import { ref, watch } from 'vue'
+
+const $emits = defineEmits<{
+  (e: 'sign-up'): void
+  (e: 'update:modelValue', value: Object): void
+}>()
+
+const form = ref({
+  email: '',
+  managerName: '',
+  phone: '',
+  restaurantName: ''
+})
+
+watch(
+  () => form.value,
+  (newValue) => {
+    if (newValue) {
+      $emits('update:modelValue', newValue)
+    }
+  },
+  { deep: true }
+)
+</script>
+
+<template>
+  <form @submit.prevent.stop="$emit('sign-up')">
+    <div class="flex flex-col w-full gap-2">
+      <div>
+        <Label for="establishment">Nome do Estabelecimento</Label>
+        <Input
+          id="establishment"
+          v-model:model-value="form.restaurantName"
+          placeholder="Nome do Estabelecimento"
+        />
+      </div>
+      <div>
+        <Label for="name">Nome</Label>
+        <Input id="name" v-model:model-value="form.managerName" placeholder="Nome" />
+      </div>
+      <div>
+        <Label for="email">E-mail</Label>
+        <Input id="email" v-model:model-value="form.email" placeholder="E-mail" />
+      </div>
+      <div>
+        <Label for="phone">Celular</Label>
+        <Input id="phone" v-model:model-value="form.phone" placeholder="Celular" />
+      </div>
+    </div>
+    <div class="flex flex-col items-center justify-center mt-5 text-center">
+      <Button class="w-full" type="submit">Finalizar Cadastro</Button>
+      <p class="px-2 text-sm text-muted-foreground mt-2">
+        Ao continuar, você concorda com nossos
+        <RouterLink class="underline underline-offset-4" :to="{ name: 'login' }">
+          termos de serviço
+        </RouterLink>
+        e
+        <RouterLink class="underline underline-offset-4" :to="{ name: 'login' }">
+          políticas de privacidade
+        </RouterLink>
+      </p>
+    </div>
+  </form>
+</template>
